e20dfac06565c7c495b4b3947b739a447478cdeb
MehradMi <165255856+MehradMi@users.noreply.github.com>
2026-02-23 11:04:42 +0330

    Added Level Scene To The Game

--- src/player.cpp
+++ src/player.cpp
@@ -1,19 +1,32 @@
 #include "player.h"
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/classes/input.hpp>
+#include <godot_cpp/classes/engine.hpp>
 
-void godot::Player::_bind_methods() {}
+void Player::_bind_methods() {}
 
-godot::Player::Player() {}
+Player::Player() {}
 
-godot::Player::~Player() {}
+Player::~Player() {}
 
-void godot::Player::_process(double delta) {
-    godot::Input *input = godot::Input::get_singleton();
-    if (input->is_action_just_pressed("ui_accept")) {
+void Player::_process(double delta) {
+    godot::Input  *input = godot::Input::get_singleton();
+    godot::Engine *engine = godot::Engine::get_singleton();
+    
+    // Check if we are currently inside the editor 
+    if (engine->is_editor_hint()) return;
+
+    if (input->is_action_pressed("ui_accept")) {
         godot::Vector3 cur_position = get_position();
         godot::Vector3 new_position = godot::Vector3(cur_position.x, cur_position.y + delta, cur_position.z);
         set_position(new_position); 
-        godot::print_line(cur_position.x, " ", cur_position.y, " ", cur_position.z);
+    }
+
+    if (input->is_action_pressed("ui_left")) {
+        rotate_z(delta); 
+    }
+
+    if (input->is_action_pressed("ui_right")) {
+        rotate_z(-delta);
     }
 }
--- src/player.h
+++ src/player.h
@@ -4,22 +4,20 @@
 #include <godot_cpp/classes/mesh_instance3d.hpp>
 #include <godot_cpp/classes/node3d.hpp>
 
-namespace godot {
-    class Player : public godot::Node3D {
-        GDCLASS(Player, Node3D);
+class Player : public godot::Node3D {
+    GDCLASS(Player, Node3D);
 
-        private:
-            godot::Vector3 cur_position;
+    private:
+        godot::Vector3 cur_position;
 
-        protected:
-            static void _bind_methods();
+    protected:
+        static void _bind_methods();
 
-        public: 
-            Player();
-            ~Player();
+    public: 
+        Player();
+        ~Player();
 
-            void _process(double delta) override;
-    };
-}
+        void _process(double delta) override;
+};
 
 #endif
